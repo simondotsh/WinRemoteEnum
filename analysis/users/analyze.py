8ef57555a7c22496ce06feede2f759d9314db9b4
simondotsh <simon747sh@gmail.com>
2022-01-07 17:55:13 -0500

    Completed users' analyze.py

--- analysis/users/analyze.py
+++ analysis/users/analyze.py
@@ -4,35 +4,93 @@ from argparse import ArgumentParser
 from os.path import isfile, expanduser
 import json
 
-from impacket.dcerpc.v5.samr import GROUP_MEMBERSHIP
+COMPROMISED_FILE = 'compromised_users.txt'
+REMOTE_ACCESS_RIDS = [544, 555, 580]
+POTENTIAL_REMOTE_ACCESS_RIDS = [559, 562]
 
-remote_access_rid = [544, 555, 580]
-uncertain_remote_access_rid = [559, 562]
+def main():
+    args = parse_args()
+    results = get_results(args.users_results)
+    users_filter = get_users(args.filter)
 
-parser = ArgumentParser()
-parser.add_argument('file', help='Path of the users.json file to analyze.')
+    for target in results:
+        target_groups = {}
+        target_groups[target['target']] = get_remote_groups(
+            target['results']['groups'], users_filter
+        )
 
-args = parser.parse_args()
-file = expanduser(args.file)
+        print(json.dumps(target_groups, indent=4))
 
-if not isfile(file):
-    print(f'{file}: No such file.')
-    exit(0)
+def parse_args():
+    parser = ArgumentParser()
+    parser.add_argument('users_results', 
+        help='Path of the users.json file to analyze.'
+    )
+    parser.add_argument('-f', '--filter', dest='filter', 
+        action='store_true', default=False,
+        help='The script will only output members of groups that are '
+             'specified in the compromised_users.txt file.'
+    )
 
-try:
-    with open(file) as f:
-        results = json.load(f)
-except json.decoder.JSONDecodeError:
-    print(f'{file}: Invalid JSON file.')
-    exit(0)
+    args = parser.parse_args()
+    return args
 
-for target in results:
-    for group in target['results']['groups']:
-        if group['rid'] in remote_access_rid:
-            print(group)
-            continue
+def get_results(users_results):
+    users_results = expanduser(users_results)
 
-        if group['rid'] in uncertain_remote_access_rid:
-            print(group)
+    if not isfile(users_results):
+        print(f'{users_results}: No such file.')
+        exit(0)
 
-            
+    try:
+        with open(users_results) as f:
+            results = json.load(f)
+    except json.decoder.JSONDecodeError:
+        print(f'{users_results}: Invalid JSON file.')
+        exit(0)
+
+    return results
+
+def get_users(filter):
+    if not filter:
+        return []
+
+    with open(COMPROMISED_FILE) as f:
+        return f.read().lower().splitlines()
+
+def get_remote_groups(groups, users_filter):
+    remote_groups = []
+
+    for group in groups:
+        if len(group['members']):
+            if group['rid'] in REMOTE_ACCESS_RIDS:
+                group['access_certainty'] = 'certain'
+            elif group['rid'] in POTENTIAL_REMOTE_ACCESS_RIDS:
+                group['access_certainty'] = 'potential'
+            else:
+                continue
+
+            if users_filter:
+                filtered_members = filter_members(group, users_filter)
+
+                if filtered_members:
+                    group['members'] = filtered_members
+                    remote_groups.append(group)
+                else:
+                    continue
+            else:
+                remote_groups.append(group)
+
+    return remote_groups
+
+def filter_members(group, users_filter):
+    members = []
+
+    for member in group['members']:
+        if member['name'].lower() in users_filter:
+            members.append(member)
+
+    return members
+    
+if __name__ == '__main__':
+    main()
