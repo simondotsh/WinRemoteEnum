09c0aafc6f8af403337a0dcaaf4d05ca8ea57a91
simondotsh <simon747sh@gmail.com>
2022-01-07 16:23:00 -0500

    Fixed inconsistency of double quotes to fit the rest of the project

--- winremoteenum.py
+++ winremoteenum.py
@@ -7,5 +7,5 @@ def main():
     config, args = Cli.parse_and_validate()
     Orchestrator.launch_modules(config, args.modules, args.targets, args.audit)
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
